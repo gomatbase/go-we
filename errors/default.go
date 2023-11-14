9e023e12df273ae6db7d8f6245c1c2b7ff51315d
GOM <gom@theba.se>
2023-11-14 11:47:03 +0100

    add 2XX and 3XX status erros as flow control events

--- errors/default.go
+++ errors/default.go
@@ -8,7 +8,32 @@ import (
 	"net/http"
 )
 
+// WE errors act both as errors as well as flow control events. Usage of we errors allow filters, for example, to stop the flow chain with an error
+// or with a normal response that should intercept the request (redirects or login forms, for example)
+// For that reason, default we errors are provided with all standard flavours of status codes.
+// Technically they are still considered an error as they represent a request that didn't follow the expected normal flow.
+
 var (
+	// Interruptions. Errors to be used as flow control events forcing a reply with a different content than the expected one. Only 2XX status
+	// that could typically be used in an interrupted flow (something that doesn't cause an error but also doesn't process the data from the request)
+	// are provided as default errors.
+	OKInterruption                   = &weError{statusCode: http.StatusOK, message: "Ok"}
+	NonAuthoritativeInfoInterruption = &weError{statusCode: http.StatusNonAuthoritativeInfo, message: "Non Authoritative Info"}
+	NoContentInterruption            = &weError{statusCode: http.StatusNoContent, message: "No Content"}
+	ResetContentInterruption         = &weError{statusCode: http.StatusResetContent, message: "Reset Content"}
+	AlreadyReportedInterruption      = &weError{statusCode: http.StatusAlreadyReported, message: "Already Reported"}
+
+	// Redirects. Errors to be used as flow control events and not necessarily as an error
+	MultipleChoicesRedirect   = &weError{statusCode: http.StatusMultipleChoices, message: "Multiple Choices"}
+	MovedPermanentlyRedirect  = &weError{statusCode: http.StatusMovedPermanently, message: "Moved Permanently"}
+	FoundRedirect             = &weError{statusCode: http.StatusFound, message: "Found"}
+	SeeOtherRedirect          = &weError{statusCode: http.StatusSeeOther, message: "See Other"}
+	NotModifiedRedirect       = &weError{statusCode: http.StatusNotModified, message: "Not Modified"}
+	UseProxyRedirect          = &weError{statusCode: http.StatusUseProxy, message: "Use Proxy"}
+	TemporaryRedirectRedirect = &weError{statusCode: http.StatusTemporaryRedirect, message: "Temporary Redirect"}
+	PermanentRedirectRedirect = &weError{statusCode: http.StatusPermanentRedirect, message: "Permanent Redirect"}
+
+	// Request Errorss
 	BadRequestError                   = &weError{statusCode: http.StatusBadRequest, message: "Bad Request"}
 	UnauthorizedError                 = &weError{statusCode: http.StatusUnauthorized, message: "Unauthorized"}
 	PaymentRequiredErrorError         = &weError{statusCode: http.StatusPaymentRequired, message: "Payment Required"}
@@ -38,6 +63,7 @@ var (
 	RequestHeaderFieldsTooLargeError  = &weError{statusCode: http.StatusRequestHeaderFieldsTooLarge, message: "Request Header Fields Too Large"}
 	UnavailableForLevalReasonsError   = &weError{statusCode: http.StatusUnavailableForLegalReasons, message: "Unavailable For Legal Reasons"}
 
+	// Server Errors
 	InternalServerError                = &weError{statusCode: http.StatusInternalServerError, message: "Internal Server Error"}
 	NotImplementedError                = &weError{statusCode: http.StatusNotImplemented, message: "Not Implemented"}
 	BadGatewayError                    = &weError{statusCode: http.StatusBadGateway, message: "Bad Gateway"}
