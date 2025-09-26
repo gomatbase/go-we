25264544ddd799bbd80809f7edaf6f3b72ccedbb
GOM <gom@theba.se>
2025-09-26 11:51:40 +0200

    add utility response json method that dumps response to stdout

--- util/response.go
+++ util/response.go
@@ -6,16 +6,31 @@ package util
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
-func ReplyJson(w http.ResponseWriter, status int, value any) error {
+func replyJson(w http.ResponseWriter, status int, value any) ([]byte, error) {
 	if body, e := json.Marshal(value); e == nil {
 		w.Header().Add("Content-type", "application/json")
 		w.WriteHeader(status)
 		if _, e = w.Write(body); e != nil {
-			return e
+			return nil, e
 		}
+		return body, nil
+	} else {
+		return nil, e
+	}
+}
+
+func ReplyJson(w http.ResponseWriter, status int, value any) error {
+	_, e := replyJson(w, status, value)
+	return e
+}
+
+func DumpedReplyJson(w http.ResponseWriter, status int, value any) error {
+	if body, e := replyJson(w, status, value); e == nil {
+		fmt.Println("response: code:", status, ", body:", string(body))
 	} else {
 		return e
 	}
